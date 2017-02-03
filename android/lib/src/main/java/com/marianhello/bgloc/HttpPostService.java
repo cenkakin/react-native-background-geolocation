8f87e1ef88a28d6411194175e0d193a270e4ca79
cenkakin <cenk.akin91@gmail.com>
2017-02-03 14:41:35 +0300

    http post service is updated for okhttp3

--- android/lib/src/main/java/com/marianhello/bgloc/HttpPostService.java
+++ android/lib/src/main/java/com/marianhello/bgloc/HttpPostService.java
@@ -1,11 +1,6 @@
 package com.marianhello.bgloc;
 
 import com.facebook.react.modules.network.OkHttpClientProvider;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,10 +8,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class HttpPostService {
 
-    public static final OkHttpClient OK_HTTP_CLIENT = OkHttpClientProvider.getOkHttpClient();
+    private static final OkHttpClient OK_HTTP_CLIENT = OkHttpClientProvider.getOkHttpClient();
 
     public static int postJSON(String url, Object json, Map<String, String> headers) throws IOException {
         Headers.Builder headersBuilder = new Headers.Builder();
@@ -25,17 +25,15 @@ public class HttpPostService {
         }
 
         Request.Builder builder = new Request.Builder();
-        RequestBody body = RequestBody.create(
-            MediaType.parse("application/json"),
-            json.toString());
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), json.toString());
         return OK_HTTP_CLIENT.newCall(builder.url(url)
-                                             .post(body)
-                                             .headers(headersBuilder.build())
-                                             .build()).execute().code();
+                .post(body)
+                .headers(headersBuilder.build())
+                .build()).execute().code();
     }
 
     public static int postFile(String url, File file, Map<String, String> headers)
-        throws Exception {
+            throws Exception {
         Request.Builder builder = new Request.Builder();
         Headers.Builder headersBuilder = new Headers.Builder();
 
@@ -43,13 +41,11 @@ public class HttpPostService {
             headersBuilder.set(pair.getKey(), pair.getValue());
         }
         String content = convertStreamToString(new FileInputStream(file));
-        RequestBody body = RequestBody.create(
-            MediaType.parse("application/json"),
-            content);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), content);
         return OK_HTTP_CLIENT.newCall(builder.url(url)
-                                             .post(body)
-                                             .headers(headersBuilder.build())
-                                             .build()).execute().code();
+                .post(body)
+                .headers(headersBuilder.build())
+                .build()).execute().code();
     }
 
     private static String convertStreamToString(InputStream is) throws Exception {
